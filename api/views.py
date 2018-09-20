50f27516b03e69cedafe9ac511a08c334b3ceb5c
handabo <530439558@qq.com>
2018-09-20 19:44:48 +0800

    添加了区域查询功能

--- api/views.py
+++ api/views.py
@@ -1,5 +1,14 @@
-from api.serializers import HouseTypeSerializer
 from rest_framework import viewsets
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+
+from api.serializers import HouseTypeSerializer
+from api.serializers import DistrictSerializer
+from api.serializers import EstateSerializer
+
+from common.models import HouseType
+from common.models import District
+from common.models import Estate
 from common.models import HouseType
 
 
@@ -7,3 +16,10 @@ class HouseTypeViewSet(viewsets.ModelViewSet):
     queryset = HouseType.objects.all()
     serializer_class = HouseTypeSerializer
     pagination_class = None
+
+
+@api_view(['GET'])
+def provinces(request):
+    query_set = District.objects.filter(parent__isnull=True)
+    serializer = DistrictSerializer(query_set, many=True)
+    return JsonResponse(serializer.data, safe=False)
--- fang/__init__.py
+++ fang/__init__.py
@@ -1,3 +1,20 @@
 import pymysql
+import celery
+import os
+
+from fang import settings
 
 pymysql.install_as_MySQLdb()
+
+# 注册环境变量
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fang.settings')
+
+app = celery.Celery('fang',
+                    backend='amqp://hanbo:123456@120.77.222.217:5672/vhost1'
+)
+
+# 从默认的配置文件读取配置信息
+app.config_from_object('django.conf:settings')
+
+# Celery加载所有注册的应用
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
